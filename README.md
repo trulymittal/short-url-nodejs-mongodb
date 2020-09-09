717a74ece3cb87dc47cf7bc436df5089e3bb5d85
Truly Mittal <trulymittal@gmail.com>
2020-09-09 18:18:57 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,49 @@
+# URL shortner application using NodeJs & MongoDB
+
+This is an example of how to make a application which shortens the url entered by the client.
+
+**MongoDB** is used for storing Users in Database.
+
+---
+
+## To start setting up the project
+
+Step 1: Clone the repo
+
+```bash
+git clone https://github.com/trulymittal/short-url-nodejs-mongodb
+```
+
+Step 2: cd into the cloned repo and run:
+
+```bash
+npm install
+```
+
+Step 3: Install & Run MongoDB (Linux Ubuntu)
+
+See <https://docs.mongodb.com/manual/installation/> for more infos
+
+Step 4 (optional): If Nodemon not installed
+
+```bash
+npm install -g nodemon
+```
+
+Step 5: Start the application by
+
+```bash
+npm start
+```
+
+## Author
+
+- [**Truly Mittal**](https://trulymittal.com)
+
+## Contribute
+
+You can fork this repo and send me a PR.
+
+## License
+
+This project is licensed under the MIT License.
---
+++ app.js
@@ -0,0 +1,76 @@
+const express = require('express')
+const shortId = require('shortid')
+const createHttpError = require('http-errors')
+const mongoose = require('mongoose')
+const path = require('path')
+const ShortUrl = require('./models/url.model')
+
+const app = express()
+app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+
+mongoose
+  .connect('mongodb://localhost:27017', {
+    dbName: 'url-shortner',
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+  })
+  .then(() => console.log('mongoose connected 💾'))
+  .catch((error) => console.log('Error connecting..'))
+
+app.set('view engine', 'ejs')
+
+app.get('/', async (req, res, next) => {
+  res.render('index')
+})
+
+app.post('/', async (req, res, next) => {
+  try {
+    const { url } = req.body
+    if (!url) {
+      throw createHttpError.BadRequest('Provide a valid url')
+    }
+    const urlExists = await ShortUrl.findOne({ url })
+    if (urlExists) {
+      res.render('index', {
+        // short_url: `${req.hostname}/${urlExists.shortId}`,
+        short_url: `${req.headers.host}/${urlExists.shortId}`,
+      })
+      return
+    }
+    const shortUrl = new ShortUrl({ url: url, shortId: shortId.generate() })
+    const result = await shortUrl.save()
+    res.render('index', {
+      // short_url: `${req.hostname}/${urlExists.shortId}`,
+      short_url: `${req.headers.host}/${result.shortId}`,
+    })
+  } catch (error) {
+    next(error)
+  }
+})
+
+app.get('/:shortId', async (req, res, next) => {
+  try {
+    const { shortId } = req.params
+    const result = await ShortUrl.findOne({ shortId })
+    if (!result) {
+      throw createHttpError.NotFound('Short url does not exist')
+    }
+    res.redirect(result.url)
+  } catch (error) {
+    next(error)
+  }
+})
+
+app.use((req, res, next) => {
+  next(createHttpError.NotFound())
+})
+
+app.use((err, req, res, next) => {
+  res.status(err.status || 500)
+  res.render('index', { error: err.message })
+})
+
+app.listen(3000, () => console.log('🌏 on port 3000...'))
